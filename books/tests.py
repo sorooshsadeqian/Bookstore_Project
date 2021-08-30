6c4014ab91b051236d200d2c7937785947a5fa46
Soroosh <sadeqiansoroosh@gmail.com>
2021-08-30 22:50:02 +0430

    chapter 10

---
+++ books/tests.py
@@ -0,0 +1,27 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Book
+
+
+class BookTests(TestCase):
+    def setUp(self):
+        self.book = Book.objects.create(title="harry potter", author="JK rowling", price="25")
+
+    def test_book_listing(self):
+        self.assertEqual(f'{self.book.title}', "harry potter")
+        self.assertEqual(f'{self.book.author}', "JK rowling")
+        self.assertEqual(f'{self.book.price}', '25')
+
+    def test_book_list_view(self):
+        response = self.client.get(reverse('book_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'harry potter')
+        self.assertTemplateUsed(response, 'books/book_list.html')
+
+    def test_book_detail_view(self):
+        response = self.client.get(self.book.get_absolute_url())
+        no_response = self.client.get('/books/12345')
+        self.assertTemplateUsed(response, 'books/book_detail.html')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, "harry potter")
--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('NEW_SECRET_KEY')
+SECRET_KEY = os.environ.get('NEW_SECRET_KEY', default="django-insecure-7-5f&fuiav@k#3f^hm&5-55u3ky2i#4g#*-ly5_3+_(wc#z_$y")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
+    'books.apps.BooksConfig',
     'django.contrib.sites',
 
     'crispy_forms',
