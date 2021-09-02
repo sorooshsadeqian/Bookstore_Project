4925a06b01a6036f363602e51b95c727e52ffd58
Soroosh <sadeqiansoroosh@gmail.com>
2021-09-03 01:00:01 +0430

    chapter 13

--- books/tests.py
+++ books/tests.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
 from django.test import TestCase, Client
 from django.urls import reverse
 from .models import Book, Review
@@ -11,6 +12,8 @@ class BookTests(TestCase):
             author="JK rowling",
             price="25"
         )
+
+        self.special_permission = Permission.objects.get(codename='special_status')
 
         self.user = get_user_model().objects.create_user(
             username="reviewuser",
@@ -29,13 +32,24 @@ class BookTests(TestCase):
         self.assertEqual(f'{self.book.author}', "JK rowling")
         self.assertEqual(f'{self.book.price}', '25')
 
-    def test_book_list_view(self):
+    def test_book_list_view_for_logged_in_user(self):
+        self.client.login(email='reviewuser@gmail.com', password="Bikarokhomm79")
         response = self.client.get(reverse('book_list'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'harry potter')
         self.assertTemplateUsed(response, 'books/book_list.html')
 
-    def test_book_detail_view(self):
+    def test_book_list_view_for_logged_out_user(self):
+        self.client.logout()
+        response = self.client.get(reverse('book_list'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '%s?next=/books/' % (reverse('account_login')))
+        response = self.client.get('%s?next=/books/' %(reverse('account_login')))
+        self.assertContains(response, 'Log In')
+
+    def test_book_detail_view_with_permission(self):
+        self.client.login(email='reviewuser@gmail.com', password='Bikarokhomm79')
+        self.user.user_permissions.add(self.special_permission)
         response = self.client.get(self.book.get_absolute_url())
         no_response = self.client.get('/books/12345')
         self.assertTemplateUsed(response, 'books/book_detail.html')
